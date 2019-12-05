7b7561add7de9bee68c5897a60e9cc86f180b612
Nickolas Iaquinta <nliaquin@svsu.edu>
2019-12-05 09:50:03 -0500

    -Added a theme module -Started chipping away at what controls will change color -Not done, still lots left to do

--- WindowsApp1/Visible Forms/Dashboard Forms/frmSettings.vb
+++ WindowsApp1/Visible Forms/Dashboard Forms/frmSettings.vb
@@ -1,30 +1,5 @@
 ﻿Public Class frmSettings
 
-
-
-#Region "Theme Code"
-    Dim defFormBackColor As Color = frmDashboard.BackColor
-    Dim defFormForeColor As Color = frmDashboard.ForeColor
-    Dim defButtonBackColor As Color = defFormBackColor
-    Dim defButtonForeColor As Color = Color.Wheat
-
-    'Figure out how to loop through all of the forms without already having instantiated them or throwing them into an array.
-    Private Sub applyLightTheme()
-
-    End Sub
-
-    Private Sub applyDarkTheme()
-
-    End Sub
-
-    Private Sub cbTheme_CheckedChanged(sender As Object, e As EventArgs) Handles cbTheme.CheckedChanged
-        If cbTheme.Checked Then
-            applyLightTheme()
-        Else
-            applyDarkTheme()
-        End If
-    End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         frmTreausryEmail.ShowDialog()
     End Sub
@@ -37,6 +12,15 @@
         frmEditAppetizerOptions.ShowDialog()
     End Sub
 
+
+#Region "Theme Code"
+    Private Sub cbTheme_CheckedChanged(sender As Object, e As EventArgs) Handles cbTheme.CheckedChanged
+        If cbTheme.Checked Then
+            ApplyLightTheme()
+        Else
+            ApplyDarkTheme()
+        End If
+    End Sub
 #End Region
 
 End Class
